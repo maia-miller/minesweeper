1183911b58c37de5ae4167666ea35b58977c5324
Maia <maiamiller25@gmail.com>
2017-12-01 12:23:49 -0500

    Added win condition message to minesweeper

--- minesweeper.js
+++ minesweeper.js
@@ -20,6 +20,8 @@ function startGame () {
   document.addEventListener('click', checkForWin);
   document.addEventListener('contextmenu', checkForWin);
 
+
+
   for (var i=0; i < board.cells.length; i++) {
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
   }
@@ -30,11 +32,21 @@ function startGame () {
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
-function checkForWin () {
 
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+// You can use this function call to declare a winner (once you've
+// detected that they've won, that is!)
+function checkForWin () {
+  for (var i = 0; i < board.cells.length; i++) {
+    if ((board.cells[i].isMine) && (!board.cells[i].isMarked)) {
+      lib.displayMessage("Keep going!");
+      return;
+    } else if (board.cells[i].hidden && (!board.cells[i].isMine)) {
+      lib.displayMessage("You're not done yet!");
+      return;
+    } else {
+     lib.displayMessage('You win!')
+    }
+  }
 }
 
 // Define this function to count the number of mines around the cell
